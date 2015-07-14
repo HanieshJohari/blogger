a71a10176c2a45c6ad321818d4e5c5f0f2ea8e59
Haniesh Johari <hanieshjohari@gmail.com>
2015-07-14 21:54:33 +0530

    set data on index and setup links to edit and delete.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,30 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def create
   end
 
   def show
+    @article = Article.find_by(params[:id])
   end
 
   def edit
+    @article = Article.find_by(params[:id])
   end
 
   def update
+    @article = Article.find_by(params[:id])
   end
 
   def destroy
+    @article = Article.find_by(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
