1d24d0db55a974653efc7c71f48debf4d78a3ef6
Haniesh Johari <hanieshjohari@gmail.com>
2015-07-15 16:16:23 +0530

    provide method definition for CRUD and generate views for Article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,10 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new create_params
+    if @article.save
+      redirect_to article_path(@article)
+    end
   end
 
   def show
@@ -20,11 +24,20 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find_by(params[:id])
+    @article.update_attributes(create_params)
+    if @article.save
+    redirect_to article_path(@article)
+    end
   end
-
+  
   def destroy
     @article = Article.find_by(params[:id])
     @article.destroy
     redirect_to articles_path
   end
+
+  def create_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
